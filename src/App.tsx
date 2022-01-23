b2b0287c59d23f7f39051fbfbe875fc12c7184c6
Josue Martinez <j.e.martinez1993@gmail.com>
2022-01-23 13:04:32 +0100

    counter starts at 42 then when you click it adds 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import "./styles.css";
 
+const {useState} = React;
+
+
 export default function App(){
+
+    const [counter, setCounter] = useState(42);
+
     return (
         <div className="App">
         <h1>Hello Coding Challenge</h1>
-        <h2>Let's start adding making some changes :D</h2>
+        <h2>
+            {counter}
+        </h2>
+        <button onClick={()=> {
+            setCounter(counter + 1);
+        }}>add one</button>
         </div>
+
+
     );
 }
+
