215cb53865504e86450bc9d764e54ebdb013ac96
Josue Martinez <j.e.martinez1993@gmail.com>
2022-01-23 12:49:47 +0100

    ready to start challenge

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import "./styles.css";
+
+export default function App(){
+    return (
+        <div className="App">
+        <h1>Hello Coding Challenge</h1>
+        <h2>Let's start adding making some changes :D</h2>
+        </div>
+    );
+}
