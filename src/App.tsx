20484a917b42ffa9076f209aa241e111828edb50
Josue Martinez <j.e.martinez1993@gmail.com>
2022-01-24 11:50:26 +0100

    adding api string to use and axios example for get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
+import axios from 'axios';
 import "./styles.css";
+
 
 const {useState} = React;
 
+/*npm example of how to use axios
+axios.get('/user?ID=12345')
+.then(function(response) {
+    //handle success
+    console.log(response);
+})
+.catch(function (error){
+    //handle error
+    console.log(error);
+})
+.then(function(){
+    //always executed
+});
+
+API we'll be using: https://randomuser.me/api
+
+*/
 
 export default function App(){
 
